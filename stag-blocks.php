e21abc7b9b9fa4c883d651965947932d1d4f23d7
Ram Ratan Maurya <ratan.maurya@gmail.com>
2018-07-08 15:26:42 +0530

    Added initial files

---
+++ stag-blocks.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Stag Blocks
+ * Plugin URI: https://github.com/mauryaratan/stag-blocks/
+ * Description: Handy WordPress editor blocks.
+ * Author: Codestag
+ * Author URI: https://codestag.com/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package SGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
