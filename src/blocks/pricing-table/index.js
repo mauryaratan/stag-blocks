00aeb5fa237fb553ff0e2909ea0a412eb4700a31
Ram Ratan Maurya <ratan.maurya@gmail.com>
2018-07-29 18:52:30 +0530

    Pricing table: Frontend implementation

--- src/blocks/pricing-table/index.js
+++ src/blocks/pricing-table/index.js
@@ -1,3 +1,4 @@
+import classnames from 'classnames';
 import Controls from './controls';
 import Edit from './edit';
 import './editor.scss';
@@ -6,6 +7,10 @@ import './style.scss';
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const { Fragment } = wp.element;
+
+const {
+	RichText,
+} = wp.editor;
 
 registerBlockType( 'sgb/pricing-table', {
 	title: __( 'Pricing table' ),
@@ -47,7 +52,7 @@ registerBlockType( 'sgb/pricing-table', {
 				{
 					title: 'Startup',
 					description: 'This is where it all begins',
-					price: '20',
+					price: '$20',
 					price_term: 'month',
 					features: [],
 					featured: false,
@@ -57,7 +62,7 @@ registerBlockType( 'sgb/pricing-table', {
 				{
 					title: 'Enterprise',
 					description: 'You know you have made it when...',
-					price: '100',
+					price: '$100',
 					price_term: 'month',
 					features: [],
 					featured: true,
@@ -80,9 +85,50 @@ registerBlockType( 'sgb/pricing-table', {
 
 	save: function( props ) {
 		const { attributes } = props;
+		const className = 'wp-block-sgb-pricing-table';
 
 		return (
-			<p>You suck</p>
+			<div className={ classnames( className, `columns-${ attributes.columns }` ) }>
+				{
+					( attributes.tables.length ) ? ( attributes.tables.map( ( table, i ) => (
+						<div
+							key={ i }
+							className={ classnames( `${ className }__table`, {
+								'is-featured': table.featured,
+							} ) }
+							style={ {
+								backgroundColor: attributes.backgroundColor,
+								color: attributes.textColor,
+							} }
+						>
+							<div className={ `${ className }__header` }>
+								<RichText.Content tagName="h3" className={ `${ className }__title` } value={ table.title } />
+								<RichText.Content tagName="p" className={ `${ className }__description` } value={ table.description } />
+							</div>
+
+							<div className={ `${ className }__price` }>
+								<RichText.Content tagName="span" className={ `${ className }__price__amount` } value={ table.price } />
+								{ ' / ' }
+								<span className={ `${ className }__price__term_selector` }>{ table.price_term }</span>
+							</div>
+
+							<RichText.Content tagName="div" className={ `${ className }__features` } value={ table.features } />
+
+							<div className={ `${ className }__footer` }>
+								<div className="wp-block-button">
+									<RichText.Content
+										tagName="a"
+										className="wp-block-button__link"
+										href={ table.buttonURL }
+										title={ table.buttonText }
+										value={ table.buttonText }
+									/>
+								</div>
+							</div>
+						</div>
+					) ) ) : null
+				}
+			</div>
 		);
 	},
 } );
