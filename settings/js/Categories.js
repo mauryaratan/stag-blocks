cc986050bf9463c0d3d6fb76732b6c7f9452c0f9
Ram Ratan Maurya <ratan.maurya@gmail.com>
2018-08-10 21:57:17 +0530

    Settings: Hide categories if no blocks found

--- settings/js/Categories.js
+++ settings/js/Categories.js
@@ -37,53 +37,55 @@ export default class Categories extends Component {
 		return (
 			<BlocksContext.Consumer>
 				{ context => (
-					<ul className="block-categories">
-						{
-							!! ( categories ) && ( categories.map( ( category ) => (
-								<li key={ category.slug }>
-									<a
-										href={ `#${ category.slug }` }
-										onClick={ ( e ) => {
-											e.preventDefault();
-											let link = new URL( e.target.href );
-											link = link.hash.slice( 1 );
-											context.setCategory( link );
-										} }
-										className={ classnames( {
-											'is-active': context.state.category === category.slug,
-										} ) }
-									>
-										{ category.title }
-									</a>
-								</li>
-							) ) )
-						}
-						<IconButton
-							label={ __( 'Search' ) }
-							onClick={ () => {
-								this.setState( { searchVisible: ! this.state.searchVisible } );
-								this.focus();
-								if ( this.state.searchVisible ) {
-									this.textInput.value = '';
-									context.resetSearch();
-								}
-							} }
-							icon={ this.state.searchVisible ? 'no' : <i className="fas fa-search" /> }
-							className="block-search-button"
-							style={ {
-								marginLeft: 'auto',
-							} }
-						/>
-						<input
-							type="text"
-							className={ classnames( 'components-text-control__input block-search', {
-								'is-visible': !! this.state.searchVisible,
-							} ) }
-							onChange={ ( event ) => context.handleSearch( event.target.value ) }
-							placeholder={ __( 'Search a block...' ) }
-							ref={ ( ref ) => ( this.textInput = ref ) }
-						/>
-					</ul>
+					context.state.blocks && context.state.blocks.length ? (
+						<ul className="block-categories">
+							{
+								!! ( categories ) && ( categories.map( ( category ) => (
+									<li key={ category.slug }>
+										<a
+											href={ `#${ category.slug }` }
+											onClick={ ( e ) => {
+												e.preventDefault();
+												let link = new URL( e.target.href );
+												link = link.hash.slice( 1 );
+												context.setCategory( link );
+											} }
+											className={ classnames( {
+												'is-active': context.state.category === category.slug,
+											} ) }
+										>
+											{ category.title }
+										</a>
+									</li>
+								) ) )
+							}
+							<IconButton
+								label={ __( 'Search' ) }
+								onClick={ () => {
+									this.setState( { searchVisible: ! this.state.searchVisible } );
+									this.focus();
+									if ( this.state.searchVisible ) {
+										this.textInput.value = '';
+										context.resetSearch();
+									}
+								} }
+								icon={ this.state.searchVisible ? 'no' : <i className="fas fa-search" /> }
+								className="block-search-button"
+								style={ {
+									marginLeft: 'auto',
+								} }
+							/>
+							<input
+								type="text"
+								className={ classnames( 'components-text-control__input block-search', {
+									'is-visible': !! this.state.searchVisible,
+								} ) }
+								onChange={ ( event ) => context.handleSearch( event.target.value ) }
+								placeholder={ __( 'Search a block...' ) }
+								ref={ ( ref ) => ( this.textInput = ref ) }
+							/>
+						</ul>
+					) : null
 				) }
 			</BlocksContext.Consumer>
 		);
