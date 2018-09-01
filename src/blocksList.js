f825442284977652019978a773a8b4fcafb59c14
Ram Ratan Maurya <ratan.maurya@gmail.com>
2018-09-01 13:58:47 +0530

    Remove blocks sync time schedule, sync on each load

--- src/blocksList.js
+++ src/blocksList.js
@@ -20,58 +20,40 @@ window.onload = () => {
 	const blocks = wp.blocks.getBlockTypes();
 	const formattedBlocks = JSON.stringify( blocks, getCircularReplacer() );
 
-	const params = window.location.search;
-	const searchParams = new URLSearchParams( params );
-	const deleteList = searchParams.get( 'refresh_blocks' );
-
-	if ( deleteList ) {
-		localStorage.removeItem( 'stagBlocksSyncTime' );
-	}
-
-	const hours = 24; // Reset when storage is more than 24hours
-	const now = new Date().getTime();
-	const stagBlocksSyncTime = localStorage.getItem( 'stagBlocksSyncTime' );
-
 	if ( ! blocks.length ) {
 		console.warn( 'No blocks found, bailing' ); // eslint-disable-line
 		return;
 	}
 
-	if ( now - stagBlocksSyncTime > hours * 60 * 60 * 1000 ) {
-		( async function syncBlocks() {
-			console.group( 'Stag Blocks' ); // eslint-disable-line
-			console.info( 'Syncing blocks data.' ); // eslint-disable-line
+	( async function syncBlocks() {
+		console.group( 'Stag Blocks' ); // eslint-disable-line
+		console.info( 'Syncing blocks data.' ); // eslint-disable-line
 
-			apiFetch( {
-				path: 'stag_blocks/v1/blocks',
-				method: 'POST',
-				body: formattedBlocks,
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			} ).then( () => {
-				apiFetch( { path: 'stag_blocks/v1/settings' } ).then( ( prevBlocks ) => {
-					const inactiveBlocks = Object.keys( prevBlocks ).filter( ( block ) => {
-						return ! prevBlocks[ block ];
-					} );
+		apiFetch( {
+			path: 'stag_blocks/v1/blocks',
+			method: 'POST',
+			body: formattedBlocks,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		} ).then( () => {
+			apiFetch( { path: 'stag_blocks/v1/settings' } ).then( ( prevBlocks ) => {
+				const inactiveBlocks = Object.keys( prevBlocks ).filter( ( block ) => {
+					return ! prevBlocks[ block ];
+				} );
 
-					inactiveBlocks.map( ( block ) => {
-						wp.blocks.unregisterBlockType( block );
-					} );
+				inactiveBlocks.map( ( block ) => {
+					wp.blocks.unregisterBlockType( block );
 				} );
 			} );
+		} );
 
-			localStorage.setItem( 'stagBlocksSyncTime', now );
+		// Show notice if it exists.
+		SGBNotice();
 
-			// Show notice if it exists.
-			SGBNotice();
-
-			console.info( 'Blocks data synced.' ); // eslint-disable-line
-			console.groupEnd(); // eslint-disable-line
-		}() );
-	} else {
-		console.log( 'Block data up to date with Stag Blocks' ); // eslint-disable-line
-	}
+		console.info( 'Blocks data synced.' ); // eslint-disable-line
+		console.groupEnd(); // eslint-disable-line
+	}() );
 };
 
 const SGBNotice = () => {
